6e6ef39c8cbcbf487b0a905f2b0e1b546291bbd7
Heorgii <rushchakheorgii@gmail.com>
2023-02-28 20:59:11 +0300

    some change

--- src/components/ListsComp/ListEditor/ListEditor.jsx
+++ src/components/ListsComp/ListEditor/ListEditor.jsx
@@ -20,21 +20,26 @@ const ListEditor = () => {
     }
 
     return (
-        <div className={css.lists_box}>
-            <form className={css.form} onSubmit={handleSubmit}>
-                <input name="listName" className={css.input_add} />
-                <button type="submit" className={css.button_add}>
-                    Add list
-                </button>
-            </form>
+        <div className={css.box}>
+            <h1 className={css.title}>My lists</h1>
+            <div className={css.lists_box}>
 
-            <form className={css.form}>
-                <input name="listName" className={css.input_filter} />
-                <button type="submit" className={css.button_filter}>
-                    Find list
-                </button>
-            </form>
+                <form className={css.form} onSubmit={handleSubmit}>
+                    <input name="listName" className={css.input_add} placeholder='List name' />
+                    <button type="submit" className={css.button_add}>
+                        Add list
+                    </button>
+                </form>
+
+                <form className={css.form}>
+                    <input name="listName" className={css.input_filter} placeholder='Search' />
+                    <button type="submit" className={css.button_filter}>
+                        Find list
+                    </button>
+                </form>
+            </div>
         </div>
+
     );
 }
 export default ListEditor;
